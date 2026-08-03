1a68e6960aeb8efb16de24d97a3c915f3d6f0797
hanage0130 <60529832+hanage0130@users.noreply.github.com>
2026-08-03 18:50:05 +0900

    fix

--- script.js
+++ script.js
@@ -1,37 +1,39 @@
 function init(x) {
-    for (let i = 0; i < 16; i++) {
+    const container = document.querySelector("#container");
+    container.innerHTML = "";
+    /*for (let i = 0; i < 16; i++) {
         const container = document.querySelector(`#row${i}`);
         if (container === null) {
             break;
         }
         container.remove();
-    }
-    for (let i = 0; i < 16; i++) {
+    }*/
+    for (let i = 0; i < x; i++) {
         const container = document.querySelector("#container");
         const content = document.createElement("div");
         content.classList.add("row");
         content.id = `row${i}`;
         container.appendChild(content);
-        for (let j = 0; j < 16; j++) {
+        for (let j = 0; j < x; j++) {
             const container = document.querySelector(`#row${i}`);
             const content = document.createElement("div");
             content.classList.add("cell");
-            content.style.borderStyle = "solid";
-            content.style.borderWidth = "1px";
-            content.style.height = `${x}px`;
-            content.style.width = `${x}px`;
+            // content.style.borderStyle = "solid";
+            // content.style.borderWidth = "1px";
+            content.style.height = `${10}px`;
+            content.style.width = `${10}px`;
             container.appendChild(content);
             content.addEventListener("mouseover", () => {
                 content.style.backgroundColor = "gray"
             })
-            content.addEventListener("mouseleave", () => {
+            /*content.addEventListener("mouseleave", () => {
                 content.style.backgroundColor = "white"
-            })
+            })*/
         }
     }
 }
 
-init(50);
+init(16);
 
 const btn = document.querySelector("#btn");
 btn.addEventListener("click", () => {
